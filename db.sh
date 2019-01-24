a50fb42c29695041bebac54d2a0288c44fbf4d76
vskitikov <skepa23@gmail.com>
2019-01-24 18:26:51 +0200

    update

--- db.sh
+++ db.sh
@@ -2,8 +2,15 @@
 
 source vars
 
-#Docker start
+if [ $(docker ps -a | grep -c mysql-test) != 1 ]; then
+echo "Docker instaling"
+docker run -d -p 3308:3306 -e MYSQL_ROOT_PASSWORD=toor  -e MYSQL_DATABASE=${database} -e MYSQL_USER=${username} -e MYSQL_PASSWORD=${password} --name mysql-test skepa23/mysql
+else
+echo "Docker already installed"
+fi
 
+#Docker start
+sleep 10
 echo "Docker start"
 docker start mysql-test
 
@@ -15,7 +22,8 @@ while true
    fi
    i=$(mysql -h ${hostname} -P ${port_test} -u ${username} -p${password} --database=${database} -e 'show tables'|grep -c Tables_in_wordpress;)
 done
-
+echo "  " >> log.txt
+echo "`date +%Y%m%d_%H%M%S` $filename" >> log.txt
 #Backup
 
 filename=${database}`date +%Y%m%d_%H%M%S`.sql
@@ -32,30 +40,40 @@ echo 'Restore complete';
 #Test DB
 
 echo "Comparison tables"
-
+echo "  " >> log.txt
+echo "Comparison tables" >> log.txt
+echo "====================================" >> log.txt
 #Source
 tables_source=$(mysql -h ${hostname} -P ${port} -u ${username} -p${password} --database=${database} -e 'show tables')
 wp_tables_source=($tables_source)
+
 #Backup
 tables_backup=$(mysql -h ${hostname} -P ${port_test} -u ${username} -p${password} --database=${database} -e 'show tables')
 wp_tables_backup=($tables_backup)
 
-
-index=0
-for item in ${wp_tables_source[*]}
+i="${#wp_tables_source[*]}"-1
+index=1
+for (( i; i>0; i-- ))
    do
-   if [ ${wp_tables_backup[$index]} == ${wp_tables_source[$index]} ]; then 
-      printf "   %s\n" $item "[OK!]"
+   if [ ${wp_tables_backup[$index]} == ${wp_tables_source[$index]} ]; then
+      printf "   %s\n" ${wp_tables_source[$index]} "[OK!]"
+      echo ${wp_tables_source[$index]} "[OK!]" >> log.txt
       index=$(($index+1))
    else
-      printf "ERROR"
+      printf "   %s\n" "ERROR"
+      echo "ERROR" >> log.txt
       index=$(($index+1))
-   fi 
+      rm $filename
+      exit 1
+   fi
 done
 
 #Test colums
 
 echo "Comparison colums"
+echo "  " >> log.txt
+echo "Comparison colums" >> log.txt
+echo "====================================" >> log.txt
 
 i="${#wp_tables_source[*]}"-1
 index=1
@@ -64,28 +82,26 @@ for (( i; i>0; i-- ))
     colums_source=$(echo "SELECT * FROM ${wp_tables_source[$index]}" | mysql -h ${hostname} -P ${port}  -u ${username} -p${password} --database=${database})
     colums_backup=$(echo "SELECT * FROM ${wp_tables_backup[$index]}" | mysql -h ${hostname} -P ${port_test} -u ${username} -p${password} --database=${database})
 sleep 1
-   if [ "${colums_source}" == "${colums_backup}" ]; then 
-      printf "   %s\n" ${wp_tables_source[$index]} "[OK!]"
+   if [ "${colums_source}" == "${colums_backup}" ]; then
+      printf "   %s\n"  ${wp_tables_source[$index]} "[OK!]"
+      echo ${wp_tables_source[$index]} "[OK!]" >> log.txt
       index=$(($index+1))
    else
-      printf "ERROR"
+      printf "   %s\n"  "ERROR"
+      echo  "ERROR" >> log.txt
       index=$(($index+1))
-   fi 
+      rm $filename
+      exit 1
+   fi
 done
 
-
-
-
-
-#     echo "Archiving"
-#     tar -cvf backups/$filename.tar.gz $filename
-#     rm $filename
-#     echo "Complete"
-#     echo "`date +%Y%m%d_%H%M%S` done $filename" >> log.txt 
-
-
-#  rm $filename
-#  echo "`date +%Y%m%d_%H%M%S` ZOPA" >> log.txt 
-
-
+echo "Archiving"
+tar -cvf backups/$filename.tar.gz $filename
+rm $filename
+echo "====================================" >> log.txt
+echo $filename.tar.gz >> log.txt
+echo "====================================" >> log.txt
+echo "Stop Docker"
 docker stop mysql-test
+
+exit 0
